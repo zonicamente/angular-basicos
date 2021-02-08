af68202ad8b99e1a8bd09d9d072b3c402a9a7f5c
Raul Garcia <zonicamente@hotmail.com>
2021-02-08 05:16:15 -0600

    Segunda carga

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbx.interfaces';
+
+
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+export class MainPageComponent {
+
+  personajes: Personaje[]=[
+    {
+      nombre:"Goku",
+      poder:15000
+    },
+    {
+      nombre:"Vegeta",
+      poder:10000
+    }
+  ];
 
 }
---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbx.interfaces';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html'
+})
+export class PersonajesComponent {
+  @Input('data') personajes:Personaje[]=[];
+}
